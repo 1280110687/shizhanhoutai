1b06a5d5934b73d5666c3078a056991523b81686
1280110687 <53083893+1280110687@users.noreply.github.com>
2019-10-21 16:26:41 +0800

    03-登录结构引入

--- src/main.js
+++ src/main.js
@@ -5,7 +5,9 @@ import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 // 引入路由模块
 import router from '@/router/index.js'
+
 Vue.use(ElementUI)
+
 Vue.config.productionTip = false
 
 new Vue({
--- src/pages/login.vue
+++ src/pages/login.vue
@@ -1,11 +1,34 @@
 <template>
-  <div class="login">登录</div>
+  <div class="login">
+    <el-form
+      :model="loginForm"
+      :rules="rules"
+      ref="loginForm"
+      class="demo-ruleForm"
+    >
+      <el-form-item label prop="username">
+        <el-input v-model="loginForm.username"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary">登陆</el-button>
+      </el-form-item>
+    </el-form>
+  </div>
 </template>
 <script>
 export default {
+  data () {
+    return {
+      loginForm: {
+        username: '',
+        password: ''
+      },
+      rules: {
 
+      }
+    }
+  }
 }
 </script>
 <style lang="less" scoped>
-
 </style>
